8d1f1822ba2134ed392a658d6f4f7e4a8a41af9c
Rupali Advirkar <rupalikatdare@gmail.com>
2012-10-08 15:46:57 +0530

    indentation

--- spec/lib/twitter_api_spec.rb
+++ spec/lib/twitter_api_spec.rb
@@ -1,22 +1,22 @@
 require 'spec_helper'
 
 describe TwitterApi do
-	class DummyClass
-	end
+	 class DummyClass
+	 end
 
-	before(:all) do
-	    @dummy = DummyClass.new
-    	@dummy.extend TwitterApi
-    	query = "ruby on rails"
+	 before(:all) do
+	   @dummy = DummyClass.new
+    @dummy.extend TwitterApi
+    query = "ruby on rails"
   		results_per_page = 100
   		@results = @dummy.twitter_search(query,results_per_page)
-  	end
+  end
 
-  	it "should search the result with given query string" do
+  it "should search the result with given query string" do
   		@results.length.should > 0
-  	end
+  end
     
-    it "should parse the results from twitter and provide array of hash of search result object" do
+  it "should parse the results from twitter and provide array of hash of search result object" do
     	parsed_results = @dummy.parse_searched_result(@results)
     	parsed_results.length.should > 0
     	parsed_results[0][:twitter_created_at].should_not be_nil
@@ -24,5 +24,5 @@ describe TwitterApi do
     	parsed_results[0][:from_user_id].should_not be_nil
     	parsed_results[0][:profile_image_url].should_not be_nil
     	parsed_results[0][:text].should_not be_nil
-	end
+	 end
 end
